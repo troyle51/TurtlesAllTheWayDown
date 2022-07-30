8e14ef9740bd312966b3e2c39fe1215eed697a28
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-30 19:49:19 -0400

    done

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -4,4 +4,16 @@ package PACKAGE_NAME;
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+    public static long factorial(int n) {
+        if(n == 1) return 1;
+        return n * factorial(n-1);
+    }
+
+    public static int gcd(int n1, int n2){
+            if(n2 != 0){
+                return gcd(n2, n1 % n2);
+            } else {
+                return n1;
+            }
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,44 @@
+import PACKAGE_NAME.Turtles;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+
+    @Test
+    public void testFactorial(){
+        long expected = Turtles.factorial(3);
+        long actual = 6;
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testFactorial2(){
+        long expected = Turtles.factorial(6);
+        long actual = 720;
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testGCD(){
+        int expected = Turtles.gcd(102, 68);
+        int actual = 34;
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testGCD2(){
+        int expected = Turtles.gcd(10, 6);
+        int actual = 2;
+
+        Assert.assertEquals(expected, actual);
+
+    }
 
 }
